cebfb8585e9093bbf676028f7f8cde7b04f9c929
xop141 <lowsens400@gmail.com>
2025-01-15 00:24:21 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: { 
+      colors: {
+        customDark: '#0F141E',
+        customwhite: '#F3F4F6',
+        Dark: 'rgba(17, 24, 39, 0.75)',
+        White:'rgba(255, 255, 255, 0.75)',
+        bordercolor: '#111827',
+
+
+      },
+  },
+  },
+  plugins: [],
+}
+
